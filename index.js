87ad8fb5f42ffdc44855d429ec5d5539af9f4652
Nikolay <kolka5999@gmail.com>
2023-10-01 18:29:32 +0500

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+import TelegramBot from "node-telegram-bot-api";
+import {config} from 'dotenv'
+
+config()
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
+async function main() {
+    try {
+        const res = await fetch('https://72dom.com/give-keys')
+        const result = await res.text()
+        if (result.indexOf('Запись на выдачу ключей закрыта') === -1) {
+            bot.sendMessage(437369676, 'Запись открылась')
+            return
+        }
+        console.log('записи еще нет')
+    } catch (e) {
+
+    }
+}
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+setInterval(()=> {
+    main()
+},getRandomArbitrary(60000, 180000))
